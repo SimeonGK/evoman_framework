b582b2099de3b02c79957a9e0347f01f13dca63a
jm2lammers <144369259+jm2lammers@users.noreply.github.com>
2023-10-14 21:05:25 +0200

    Add files via upload

---
+++ Results_task_II.py
@@ -0,0 +1,221 @@
+###############################################################################
+# Evoman assignment 			                                              #
+# Author: Julia Lammers        			                                      #
+# 21-09-2023                    			                                  #
+###############################################################################
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+plots_dir = './Plots_Task_II'
+#runs = 10
+generations = 100
+if not os.path.exists(plots_dir):
+    os.makedirs(plots_dir)
+
+fig, axs = plt.subplots(1, 2, figsize=(8, 2.75), constrained_layout=True, gridspec_kw={'wspace': 0.03, 'left': 0.04}, sharey=True, facecolor='whitesmoke')
+fig.text(0.005, 0.5, 'Fitness', va='center', ha = 'center', rotation='vertical', fontsize=10)
+#fig.suptitle("Population Evolution of Generalist Agents Using 2 Evolutionary Algorithms", fontsize=12, fontweight='bold', x=0.45, y=0.99, va='center')
+
+###### FOR NOW ###### :
+# Choose 2 test-results for comparison
+comparison = ['Results_Task_II/test_8_enemies_pt1', 'Results_Task_II/test_npop_200']
+
+for idx, group in enumerate(comparison):   # Now these are 2 results to compare, later we want these to be 2 groups of enemies
+    avg_mean_total = []
+    std_mean_total = []
+    avg_best_total = []
+    std_best_total = []
+
+    """"
+    for method in (1,2): #This is going to be Neural network vs NEAT. Now it is only one method.
+
+        
+        # Make a list of all directories with the results of 10 runs
+        fitness_dir = []
+        for run in range(1, runs+1):
+            directory = f'method{method}_group{group}_run{run}'
+            fitness_dir.append(directory)
+        """
+    fitness_dir = [group]
+
+    dict = {directory: {"gen": [], "best": [], "mean": [], "std": []} for directory in fitness_dir}
+    avg_mean = defaultdict(list)
+    avg_best = defaultdict(list)
+    avg_std = defaultdict(list)
+
+    for directory in fitness_dir:
+        file_path = os.path.join(directory, "results.txt")
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+
+            # Extract data from each line (if they contain floats)
+            for line in lines:
+                parts = line.strip().split()
+                try:
+                    parts = [float(item) for item in parts]
+                    gen, best, mean, std = map(float, parts)
+                    dict[directory]["gen"].append(gen)
+                    dict[directory]["best"].append(best)
+                    dict[directory]["mean"].append(mean)
+                    dict[directory]["std"].append(std)
+
+                    avg_mean[gen].append(mean)
+                    avg_best[gen].append(best)
+                    avg_std[gen].append(std)
+
+                except ValueError:
+                    pass
+
+    # Calculate means per generation
+    x_values = list(avg_mean.keys())
+    avg_mean_values = [np.mean(avg_mean[gen]) for gen in x_values]
+    std_mean_values = [np.std(avg_mean[gen]) for gen in x_values]
+    avg_best_values = [np.mean(avg_best[gen]) for gen in x_values]
+    std_best_values = [np.std(avg_best[gen]) for gen in x_values]
+
+    # Store both methods in lists
+    avg_mean_total.append(avg_mean_values)
+    std_mean_total.append(std_mean_values)
+    avg_best_total.append(avg_best_values)
+    std_best_total.append(std_best_values)
+
+    axs[idx].plot(x_values, avg_mean_total[0], label='Method 1 Mean', color='C4', linewidth=1)
+    axs[idx].plot(x_values, avg_best_total[0], label='Method 1 Best', color='C6', linewidth=1)
+    axs[idx].fill_between(x_values,
+                          np.array(avg_mean_total[0]) - np.array(std_mean_total[0]),
+                          np.array(avg_mean_total[0]) + np.array(std_mean_total[0]),
+                          color='C4', alpha=0.2, linewidth=0.5)
+    axs[idx].fill_between(x_values,
+                          np.array(avg_best_total[0]) - np.array(std_best_total[0]),
+                          np.array(avg_best_total[0]) + np.array(std_best_total[0]),
+                          color='C6', alpha=0.2, linewidth=0.5)
+
+    # Set plot labels and title
+    axs[idx].set_xlim(0, generations)
+    axs[idx].set_xlabel('Generation ($\mathit{n}$)', fontsize=9, labelpad=3)
+    axs[idx].set_ylim(0, generations)
+    axs[idx].tick_params(axis='both', labelsize=7)
+    axs[idx].set_title(f'Enemy group {group}', fontsize=10)
+    axs[idx].grid(True, linestyle='--', alpha=0.6)
+
+axs[1].legend(fontsize=9, loc='lower right')
+
+# Save and show the plot
+#fig.tight_layout()
+fig.savefig(os.path.join(plots_dir, 'Test_plot_npop_200.png'), dpi=300, bbox_inches='tight')
+fig.show()
+
+"""
+
+runs = 10
+save_dir = './Plots_Task_II'
+
+# Create a single figure with three subplots
+fig, axs = plt.subplots(1, 3, figsize=(8, 2.75), constrained_layout=True, gridspec_kw={'wspace': 0.03, 'left': 0.04}, sharey=True, facecolor='whitesmoke')
+fig.text(0.005, 0.5, 'Fitness', va='center', ha = 'center', rotation='vertical', fontsize=10)
+fig.suptitle("Population Evolution of Specialist Agents Using 2 Evolutionary Algorithms", fontsize=12, fontweight='bold', x=0.45, y=0.99, va='center')
+
+# Loop over 3 enemies
+for idx, enemy in enumerate([2, 5, 8]):
+    # Store mean & best values per method in lists
+    avg_mean_total = []
+    std_mean_total = []
+    avg_best_total = []
+    std_best_total = []
+
+    # Extract data per method out of result dictionaries and plot
+    for method in (1,2):
+        # Make a list of all directories with the results of 10 runs
+        fitness_dir = []
+        for run in range(1, runs+1):
+            directory = f'method{method}_enemy{enemy}_run{run}'
+            fitness_dir.append(directory)
+
+        # Initialize empty dictionaries to store the data from each file
+        dict = {directory: {"gen": [], "best": [], "mean": [], "std": []} for directory in fitness_dir}
+        avg_mean = defaultdict(list)
+        avg_best = defaultdict(list)
+        avg_std = defaultdict(list)
+
+        for directory in fitness_dir:
+                file_path = os.path.join(directory,"results.txt")
+                with open(file_path, "r") as file:
+                    lines = file.readlines()
+
+                    # Extract data from each line (if they contain floats)
+                    for line in lines:
+                        parts = line.strip().split()
+                        try:
+                            parts = [float(item) for item in parts]
+                            gen, best, mean, std = map(float, parts)
+                            dict[directory]["gen"].append(gen)
+                            dict[directory]["best"].append(best)
+                            dict[directory]["mean"].append(mean)
+                            dict[directory]["std"].append(std)
+
+                            avg_mean[gen].append(mean)
+                            avg_best[gen].append(best)
+                            avg_std[gen].append(std)
+
+                        except ValueError:
+                            pass
+
+        # Calculate means per generation
+        x_values = list(avg_mean.keys())
+        avg_mean_values = [np.mean(avg_mean[gen]) for gen in x_values]
+        std_mean_values = [np.std(avg_mean[gen]) for gen in x_values]
+        avg_best_values = [np.mean(avg_best[gen]) for gen in x_values]
+        std_best_values = [np.std(avg_best[gen]) for gen in x_values]
+
+        # Store both methods in lists
+        avg_mean_total.append(avg_mean_values)
+        std_mean_total.append(std_mean_values)
+        avg_best_total.append(avg_best_values)
+        std_best_total.append(std_best_values)
+
+    # Make plot per method
+    width = 1
+    height = 1.5
+
+    # Create a line plot for method 1
+    axs[idx].plot(x_values, avg_mean_total[0], label='Method 1 Mean', color='C4', linewidth=1)
+    axs[idx].plot(x_values, avg_best_total[0], label='Method 1 Best', color='C6', linewidth=1)
+    axs[idx].fill_between(x_values,
+                          np.array(avg_mean_total[0]) - np.array(std_mean_total[0]),
+                          np.array(avg_mean_total[0]) + np.array(std_mean_total[0]),
+                          color='C4', alpha=0.2, linewidth=0.5)
+    axs[idx].fill_between(x_values,
+                          np.array(avg_best_total[0]) - np.array(std_best_total[0]),
+                          np.array(avg_best_total[0]) + np.array(std_best_total[0]),
+                          color='C6', alpha=0.2, linewidth=0.5)
+
+    # Create a lineplot for method 2
+    axs[idx].plot(x_values, avg_mean_total[1], label='Method 2 Mean', color='C0', linewidth=1)
+    axs[idx].plot(x_values, avg_best_total[1], label='Method 2 Best', color='C2', linewidth=1)
+    axs[idx].fill_between(x_values, np.array(avg_mean_total[1]) - np.array(std_mean_total[1]),
+                          np.array(avg_mean_total[1]) + np.array(std_mean_total[1]),
+                          color='C0', alpha=0.2, linewidth=0.5)
+    axs[idx].fill_between(x_values, np.array(avg_best_total[1]) - np.array(std_best_total[1]),
+                          np.array(avg_best_total[1]) + np.array(std_best_total[1]),
+                          color='C2', alpha=0.2, linewidth=0.5)
+
+    # Set plot labels and title
+    axs[idx].set_xlim(0, 24)
+    axs[idx].set_xlabel('Generation ($\mathit{n}$)', fontsize=9, labelpad=3)
+    axs[idx].set_ylim(0, 100)
+    axs[idx].tick_params(axis='both', labelsize=7)
+    axs[idx].set_title(f'Enemy {enemy}', fontsize=10)
+    axs[idx].grid(True, linestyle='--', alpha=0.6)
+
+    # Add legend in the third plot
+    if idx == 2:
+        axs[2].legend(fontsize=9, loc = 'lower right')
+
+# Save and show the plot
+fig.tight_layout()
+fig.savefig(os.path.join(save_dir, 'Test_plot_Crossover_vs_blend.png'), dpi=300, bbox_inches='tight')
+fig.show()
+"""
