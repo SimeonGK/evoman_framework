46f3bc0d99da36369624f8f06506dab94a562ac5
Simeon <s.g.klumperbeek@gmail.com>
2023-10-18 18:41:01 +0200

    Add results from NEAT

--- Results_taskII_lineplots.py
+++ Results_taskII_lineplots.py
@@ -19,7 +19,7 @@ fig, axs = plt.subplots(1, 2, figsize=(8, 2.75), constrained_layout=True, gridsp
 fig.text(0.005, 0.5, 'Fitness', va='center', ha = 'center', rotation='vertical', fontsize=10)
 fig.suptitle("Population Evolution of Generalist Agents", fontsize=12, fontweight='bold',va='center')
 
-
+directory_name = ['./Final_Results_Task_II','./Final_Results_Task_II_NEAT_2']
 for idx, group in enumerate((('1 t/m 8'),('1,2,3,7'))):   # Now these are 2 results to compare, later we want these to be 2 groups of enemies
     avg_mean_total = []
     std_mean_total = []
@@ -28,65 +28,74 @@ for idx, group in enumerate((('1 t/m 8'),('1,2,3,7'))):   # Now these are 2 resu
     groupnr = idx + 1
 
     #for method in (1,2): #This is going to be Neural network vs NEAT. Now it is only one method. # TODO
+    for i in range(2):
 
-    # Make a list of all directories with the results of 10 runs
-    fitness_dir = []
-    for run in range(1, runs+1):
-        directory = f'./Final_Results_Task_II/TaskII_group{groupnr}_{run}'
-        fitness_dir.append(directory)
+        # Make a list of all directories with the results of 10 runs
+        fitness_dir = []
+        for run in range(1, runs+1):
+            directory = f'{directory_name[i]}/TaskII_group{groupnr}_{run}'
+            fitness_dir.append(directory)
 
-    dict = {directory: {"gen": [], "best": [], "mean": [], "std": []} for directory in fitness_dir}
-    avg_mean = defaultdict(list)
-    avg_best = defaultdict(list)
-    avg_std = defaultdict(list)
+        dict = {directory: {"gen": [], "best": [], "mean": [], "std": []} for directory in fitness_dir}
+        avg_mean = defaultdict(list)
+        avg_best = defaultdict(list)
+        avg_std = defaultdict(list)
 
-    for directory in fitness_dir:
-        file_path = os.path.join(directory, "results.txt")
-        with open(file_path, "r") as file:
-            lines = file.readlines()
+        for directory in fitness_dir:
+            file_path = os.path.join(directory, "results.txt")
+            with open(file_path, "r") as file:
+                lines = file.readlines()
 
-            # Extract data from each line (if they contain floats)
-            for line in lines:
-                parts = line.strip().split()
-                try:
-                    parts = [float(item) for item in parts]
-                    gen, best, mean, std = map(float, parts)
-                    dict[directory]["gen"].append(gen)
-                    dict[directory]["best"].append(best)
-                    dict[directory]["mean"].append(mean)
-                    dict[directory]["std"].append(std)
+                # Extract data from each line (if they contain floats)
+                for line in lines:
+                    parts = line.strip().split()
+                    try:
+                        parts = [float(item) for item in parts]
+                        gen, best, mean, std = map(float, parts)
+                        dict[directory]["gen"].append(gen)
+                        dict[directory]["best"].append(best)
+                        dict[directory]["mean"].append(mean)
+                        dict[directory]["std"].append(std)
 
-                    avg_mean[gen].append(mean)
-                    avg_best[gen].append(best)
-                    avg_std[gen].append(std)
+                        avg_mean[gen].append(mean)
+                        avg_best[gen].append(best)
+                        avg_std[gen].append(std)
 
-                except ValueError:
-                    pass
+                    except ValueError:
+                        pass
 
-    # Calculate means per generation
-    x_values = list(avg_mean.keys())
-    avg_mean_values = [np.mean(avg_mean[gen]) for gen in x_values]
-    std_mean_values = [np.std(avg_mean[gen]) for gen in x_values]
-    avg_best_values = [np.mean(avg_best[gen]) for gen in x_values]
-    std_best_values = [np.std(avg_best[gen]) for gen in x_values]
+        # Calculate means per generation
+        x_values = list(avg_mean.keys())
+        avg_mean_values = [np.mean(avg_mean[gen]) for gen in x_values]
+        std_mean_values = [np.std(avg_mean[gen]) for gen in x_values]
+        avg_best_values = [np.mean(avg_best[gen]) for gen in x_values]
+        std_best_values = [np.std(avg_best[gen]) for gen in x_values]
 
-    # Store both methods in lists
-    avg_mean_total.append(avg_mean_values)
-    std_mean_total.append(std_mean_values)
-    avg_best_total.append(avg_best_values)
-    std_best_total.append(std_best_values)
+        # Store both methods in lists
+        avg_mean_total.append(avg_mean_values)
+        std_mean_total.append(std_mean_values)
+        avg_best_total.append(avg_best_values)
+        std_best_total.append(std_best_values)
 
     axs[idx].plot(x_values, avg_mean_total[0], label='Method 1 Mean', color='C4', linewidth=1)
     axs[idx].plot(x_values, avg_best_total[0], label='Method 1 Best', color='C6', linewidth=1)
     axs[idx].fill_between(x_values,
-                          np.array(avg_mean_total[0]) - np.array(std_mean_total[0]),
-                          np.array(avg_mean_total[0]) + np.array(std_mean_total[0]),
-                          color='C4', alpha=0.2, linewidth=0.5)
+                        np.array(avg_mean_total[0]) - np.array(std_mean_total[0]),
+                        np.array(avg_mean_total[0]) + np.array(std_mean_total[0]),
+                        color='C4', alpha=0.2, linewidth=0.5)
     axs[idx].fill_between(x_values,
-                          np.array(avg_best_total[0]) - np.array(std_best_total[0]),
-                          np.array(avg_best_total[0]) + np.array(std_best_total[0]),
-                          color='C6', alpha=0.2, linewidth=0.5)
-
+                        np.array(avg_best_total[0]) - np.array(std_best_total[0]),
+                        np.array(avg_best_total[0]) + np.array(std_best_total[0]),
+                        color='C6', alpha=0.2, linewidth=0.5)
+    # Create a lineplot for method 2
+    axs[idx].plot(x_values, avg_mean_total[1], label='Method 2 Mean', color='C0', linewidth=1)
+    axs[idx].plot(x_values, avg_best_total[1], label='Method 2 Best', color='C2', linewidth=1)
+    axs[idx].fill_between(x_values, np.array(avg_mean_total[1]) - np.array(std_mean_total[1]),
+                        np.array(avg_mean_total[1]) + np.array(std_mean_total[1]),
+                        color='C0', alpha=0.2, linewidth=0.5)
+    axs[idx].fill_between(x_values, np.array(avg_best_total[1]) - np.array(std_best_total[1]),
+                        np.array(avg_best_total[1]) + np.array(std_best_total[1]),
+                        color='C2', alpha=0.2, linewidth=0.5)
     # Set plot labels and title
     axs[idx].set_xlim(0, generations)
     axs[idx].set_xlabel('Generation ($\mathit{n}$)', fontsize=9, labelpad=3)
@@ -95,10 +104,10 @@ for idx, group in enumerate((('1 t/m 8'),('1,2,3,7'))):   # Now these are 2 resu
     axs[idx].set_title(f'Group {groupnr} ({group}a)', fontsize=10)
     axs[idx].grid(True, linestyle='--', alpha=0.6)
 
-axs[1].legend(fontsize=9, loc='lower right')
+    axs[1].legend(fontsize=9, loc='lower right')
 
 # Save and show the plot
 #fig.tight_layout()
-fig.savefig(os.path.join(plots_dir, 'Neural network lineplots.png'), dpi=300, bbox_inches='tight')
+fig.savefig(os.path.join(plots_dir, 'Neural network lineplots_2.png'), dpi=300, bbox_inches='tight')
 fig.show()
 
--- optimization_generalist_NEAT_group68.py
+++ optimization_generalist_NEAT_group68.py
@@ -7,7 +7,6 @@
 
 # imports framework
 import sys
-
 from evoman.environment import Environment
 from demo_controller import player_controller
 from NEAT_controller import NEAT_controller
@@ -16,12 +15,15 @@ import numpy as np
 import os
 import configparser
 import neat
+import pickle
+import statistics
 config = configparser.ConfigParser()
 
 # Runs simulation
 def simulation(env: Environment, genome) -> float:
     f, p, e, t = env.play(pcont=genome)
-    return f
+    return f, p, e
+
 
 
 def main():
@@ -29,39 +31,89 @@ def main():
     headless = True
     if headless:
         os.environ["SDL_VIDEODRIVER"] = "dummy"
-
-
-    experiment_name = f'method_general_NEAT'  # Change the experiment name
-    if not os.path.exists(experiment_name):
-        os.makedirs(experiment_name)
-
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "neat.ini")
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
-    save_path = os.path.join(local_dir, experiment_name)
 
-    # initializes simulation in individual evolution mode, for single static enemy.
-    env = Environment(experiment_name=experiment_name,
-                    enemies=[1,2,3,4,5,6,7,8], #[1,2,3,7]
-                    multiplemode="yes",
-                    playermode="ai",
-                    enemymode="static",
-                    player_controller= NEAT_controller(config=config),
-                    level=2,
-                    speed="fastest",
-                    visuals=False)
-    def eval_genomes(genomes, config):
-        for genome_id, genome in genomes:
-            genome.fitness = simulation(env,genome)
-    def run_neat(config):
-        p = neat.Population(config)
-        p.add_reporter(neat.StdOutReporter(True))
-        stats = neat.StatisticsReporter()
-        p.add_reporter(stats)
-        p.add_reporter(neat.Checkpointer(1,filename_prefix=save_path))
-        
-        winner = p.run(eval_genomes, 10) #100 Generaties
-    run_neat(config)
+    for nr, group in enumerate(([1,2,3,4,5,6,7,8],[1,2,3,7])):   # We compare two groups of enemies. 2nd group reflected a broad type of enemies.
+        nr = nr+1
+        for run_variable in range(1,11):     # 10 runs per agent
+            if not os.path.exists('Final_Results_Task_II_NEAT_2'):
+                os.makedirs('Final_Results_Task_II_NEAT_2')
+            directory_name = f'TaskII_group{nr}_{run_variable}'
+            experiment_name = f'./Final_Results_Task_II_NEAT_2/{directory_name}'
+            if not os.path.exists(experiment_name):
+                os.makedirs(experiment_name)
+
+            env = Environment(experiment_name=experiment_name,
+                            enemies=group, #[1,2,3,7]
+                            multiplemode="yes",
+                            playermode="ai",
+                            enemymode="static",
+                            player_controller= NEAT_controller(config=config),
+                            level=2,
+                            speed="fastest",
+                            visuals=False)
+            run_mode = 'test'
+            #Function that evaluates all genomes
+            def eval_genomes(genomes,config):
+                for genome_id, genome in genomes:
+                    genome.fitness = simulation(env,genome)[0]
+            #Function that writes the appropiate statistics to the results.txt file
+            def write_statistics_to_file(fitness_values, gen):
+                file_path = os.path.join(experiment_name, 'results.txt')
+                # Check if we need to write the header
+                write_header = not os.path.exists(file_path)
+                # Open the file for appending
+                with open(file_path, 'a') as file:
+                    # Write the header if the file didn't exist
+                    if write_header:
+                        file.write('gen best mean std\n')
+
+                    best = max(fitness_values)
+                    mean = statistics.mean(fitness_values)
+                    std = statistics.stdev(fitness_values)
+
+                    # Append the statistics to the file
+                    file.write(f'{gen} {best:.6f} {mean:.6f} {std:.6f}\n')
+                print(f"Statistics for gen {gen} written to {file_path}")
+            #Function to run the NEAT algorithm
+            def run_neat(config):
+                stats = neat.StatisticsReporter()
+                checkpointer = neat.Checkpointer(generation_interval=10, time_interval_seconds=None, filename_prefix=experiment_name+'/neat-checkpoint-')
+                p = neat.Population(config)
+                # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-3')
+                p.add_reporter(checkpointer)
+                p.add_reporter(stats)
+                p.add_reporter(neat.StdOutReporter(True))
+                
+                winner = p.run(eval_genomes, 100) #100 Generaties
+                #Save winner
+                with open(experiment_name+"/best.pickle", "wb") as f:
+                    pickle.dump(winner,f)
+                # Save statistics
+                stats.get_fitness_stat(write_statistics_to_file)
+            
+            if run_mode == 'test':
+                        with open(experiment_name+"/best.pickle", 'rb') as f:
+                            best_genome = pickle.load(f)
+                        
+                        print(f'\n RUNNING THE BEST SAVED SOLUTION FROM {experiment_name}')
+                        # env.update_parameter('speed', 'normal')
+                        gain = []
+
+                        # 5 test-rounds of the best result
+                        for i in range(5):
+                            f,p,e = simulation(env,best_genome)
+                            gain.append(p-e)
+
+                        mean_gain = np.mean(gain)
+
+                        gain_file = open(experiment_name+'/mean_gain.txt','w')
+                        gain_file.write(str(mean_gain))
+                        gain_file.close()
+            # run_neat(config)
+    
 
 
 
